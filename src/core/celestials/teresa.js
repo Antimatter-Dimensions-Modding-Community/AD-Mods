f7f31694acff3dc684fe9aa84e82e798694429a5
antimatter19728 <antimatter19728@163.com>
2024-09-09 13:06:11 +0800

    Update teresa.js

--- src/core/celestials/teresa.js
+++ src/core/celestials/teresa.js
@@ -1,173 +1,437 @@
-import { BitUpgradeState, RebuyableMechanicState } from "../game-mechanics";
-import { DC } from "../constants";
-import { GameDatabase } from "../secret-formula/game-database";
+import { cloneDeep } from "lodash";
 
-import { Quotes } from "./quotes";
-
-export const Teresa = {
-  timePoured: 0,
-  lastUnlock: "effarig",
-  pouredAmountCap: 1e24,
-  displayName: "Teresa",
-  possessiveName: "Teresa's",
-  get isUnlocked() {
-    return Achievement(147).isUnlocked;
-  },
-  pourRM(diff) {
-    if (this.pouredAmount >= Teresa.pouredAmountCap) return;
-    this.timePoured += diff;
-    const rm = Currency.realityMachines.value.max(1e100);
-    const rmPoured = Math.min((this.pouredAmount + 1e6) * 0.01 * Math.pow(this.timePoured, 2), rm.toNumber());
-    this.pouredAmount += Math.min(rmPoured, Teresa.pouredAmountCap - this.pouredAmount);
-    Currency.realityMachines.subtract(rmPoured);
-    this.checkForUnlocks();
-  },
-  checkForUnlocks() {
-    for (const info of TeresaUnlocks.all) {
-      info.unlock();
-    }
-  },
-  initializeRun() {
-    clearCelestialRuns();
-    player.celestials.teresa.run = true;
-  },
-  rewardMultiplier(antimatter) {
-    return Decimal.max(Decimal.pow(antimatter.plus(1).log10().div(1.5e8), 12), 1);
-  },
-  get pouredAmount() {
-    return player.celestials.teresa.pouredAmount;
-  },
-  set pouredAmount(amount) {
-    player.celestials.teresa.pouredAmount = amount;
-  },
-  get fill() {
-    return Math.min(Math.log10(this.pouredAmount) / 24, 1);
-  },
-  get possibleFill() {
-    return Decimal.min(Currency.realityMachines.value.plus(this.pouredAmount).max(1).log10().div(24), 1).toNumber();
-  },
-  get rmMultiplier() {
-    return Decimal.max(250 * Math.pow(this.pouredAmount / 1e24, 0.1), 1);
-  },
-  get runRewardMultiplier() {
-    return this.rewardMultiplier(player.celestials.teresa.bestRunAM);
-  },
-  get isRunning() {
-    return player.celestials.teresa.run;
-  },
-  get runCompleted() {
-    return player.celestials.teresa.bestRunAM.gt(1);
-  },
-  quotes: Quotes.teresa,
-  symbol: "Ϟ",
-
-  reset() {
-    player.celestials.teresa.pouredAmount = 0;
-    player.celestials.teresa.unlockBits = 0;
-    player.celestials.teresa.run = false;
-    player.celestials.teresa.bestRunAM = DC.D1;
-    player.celestials.teresa.bestAMSet = [];
-    player.celestials.teresa.perkShop = [DC.D0, DC.D0, DC.D0, DC.D0, DC.D0, DC.D0];
-    player.celestials.teresa.lastRepeatedMachines = DC.D0;
-    player.celestials.teresa.lastRepeatediM = DC.D0;
-  },
+window.PRESTIGE_EVENT = {
+  DIMENSION_BOOST: 0,
+  ANTIMATTER_GALAXY: 1,
+  INFINITY: 2,
+  ETERNITY: 3,
+  REALITY: 4,
 };
 
-class PerkShopUpgradeState extends RebuyableMechanicState {
-  constructor(config) {
-    super(config);
-    this.costCap = config.costCap;
-  }
-
-  get currency() {
-    return Currency.perkPoints;
-  }
-
-  get boughtAmount() {
-    return player.celestials.teresa.perkShop[this.id];
-  }
-
-  set boughtAmount(value) {
-    player.celestials.teresa.perkShop[this.id] = value;
-  }
-
-  get isCapped() {
-    return Decimal.gte(this.cost, this.costCap());
-  }
-
-  get isAvailableForPurchase() {
-    const otherReq = this.config.otherReq ? this.config.otherReq() : true;
-    return this.currency.value.gte(this.cost) && otherReq;
-  }
-
-  onPurchased() {
-    if (this.id === 0) {
-      GameCache.staticGlyphWeights.invalidate();
-    }
-    if (this.id === 1) {
-      Autobuyer.reality.bumpAmount(2);
-    }
-    // Give a single music glyph
-    if (this.id === 4 && !Pelle.isDoomed) {
-      if (GameCache.glyphInventorySpace.value === 0) {
-        // Refund the perk point if they didn't actually get a glyph
-        Currency.perkPoints.add(1);
-        GameUI.notify.error("You have no empty inventory space!");
-      } else {
-        Glyphs.addToInventory(GlyphGenerator.musicGlyph());
-        GameUI.notify.success("Created a Music Glyph");
-      }
-    }
-    // Fill the inventory with music glyphs
-    if (this.id === 5 && !Pelle.isDoomed) {
-      const toCreate = GameCache.glyphInventorySpace.value;
-      for (let count = 0; count < toCreate; count++) Glyphs.addToInventory(GlyphGenerator.musicGlyph());
-      GameUI.notify.success(`Created ${quantifyInt("Music Glyph", toCreate)}`);
-    }
-  }
+function deepFreeze(obj) {
+  Object.keys(obj).forEach(prop => {
+    const reference = obj[prop];
+    if (typeof reference === "object") deepFreeze(reference);
+  });
+  return Object.freeze(obj);
 }
 
-class TeresaUnlockState extends BitUpgradeState {
-  get bits() { return player.celestials.teresa.unlockBits; }
-  set bits(value) { player.celestials.teresa.unlockBits = value; }
+export const DC = deepFreeze({
+  // Naming Scheme:
+  // D[0-9]: Decimal mantissa variable
+  // _: decimal (.) part of the mantissa
+  // E[0-9]: Decimal exponent variable
+  // C: Calculation. D - .div, P - .pow
+  // There are special values
 
-  get price() {
-    return this.config.price;
-  }
+  /* eslint-disable key-spacing */
+  DM1:                  new Decimal("-1"),
+  D0:                   new Decimal("0"),
 
-  get pelleDisabled() {
-    return Pelle.isDoomed && this.config.isDisabledInDoomed;
-  }
+  D0_01:                new Decimal("0.01"),
+  D0_1:                 new Decimal("0.1"),
+  D0_4:                 new Decimal("0.4"),
+  D0_5:                 new Decimal("0.5"),
+  D0_55:                new Decimal("0.55"),
+  D0_8:                 new Decimal("0.8"),
+  D0_8446303389034288:  new Decimal("0.8446303389034288"),
+  D0_95:                new Decimal("0.95"),
+  D0_965:               new Decimal("0.965"),
+  D1:                   new Decimal("1"),
+  D1_0000109:           new Decimal("1.0000109"),
+  D1_00038:             new Decimal("1.00038"),
+  D1_0004:              new Decimal("1.0004"),
+  D1_0025:              new Decimal("1.0025"),
+  D1_005:               new Decimal("1.005"),
+  D1_007:               new Decimal("1.007"),
+  D1_02:                new Decimal("1.02"),
+  D1_0285:              new Decimal("1.0285"),
+  D1_2:                 new Decimal("1.2"),
+  D1_3:                 new Decimal("1.3"),
+  D1_35:                new Decimal("1.35"),
+  D2:                   new Decimal("2"),
+  D3:                   new Decimal("3"),
+  D4:                   new Decimal("4"),
+  D5:                   new Decimal("5"),
+  D6:                   new Decimal("6"),
+  D6_66:                new Decimal("6.66"),
+  D7:                   new Decimal("7"),
+  D8:                   new Decimal("8"),
+  D9:                   new Decimal("9"),
+  D10:                  new Decimal("10"),
+  D11:                  new Decimal("11"),
+  D12:                  new Decimal("12"),
+  D13:                  new Decimal("13"),
+  D14:                  new Decimal("14"),
+  D15:                  new Decimal("15"),
+  D16:                  new Decimal("16"),
+  D17:                  new Decimal("17"),
+  D18:                  new Decimal("18"),
+  D19:                  new Decimal("19"),
+  D20:                  new Decimal("20"),
+  D60:                  new Decimal("60"),
+  D80:                  new Decimal("80"),
+  D99:                  new Decimal("99"),
+  D200:                 new Decimal("200"),
+  D11111:               new Decimal("11111"),
+  D3E4:                 new Decimal("30000"),
+  D2E5:                 new Decimal("2e5"),
+  D2E6:                 new Decimal("2e6"),
+  D5E7:                 new Decimal("5e7"),
+  D2E9:                 new Decimal("2e9"),
+  D2E25:                new Decimal("2e25"),
+  D2E22222:             new Decimal("2e22222"),
+  D9_99999E999:         new Decimal("9.99999e999"),
+  D9_9999E9999:         new Decimal("9.9999e9999"),
 
-  get isEffectActive() {
-    return !this.pelleDisabled;
-  }
+  // Calculations for precise numbers.
+  C1D1_1245:                Decimal.div(1, 1.1245),
+  D2P30D0_61:               Decimal.pow(2, 30 / 0.61),
+  C2P30:                    Decimal.pow(2, 30),
+  C2P1024:                  Decimal.pow(2, 1024),
+  C10P16000D3:              Decimal.pow(10, 16000 / 3),
+  C1D1_11888888:        new Decimal(1 / 1.11888888),
+  C1D1_11267177:        new Decimal(1 / 1.11267177),
 
-  get canBeUnlocked() {
-    return !this.isUnlocked && Teresa.pouredAmount >= this.price;
-  }
+  // 1e1 is 10
+  E1:                   new Decimal("1e1"),
+  E2:                   new Decimal("1e2"),
+  E3:                   new Decimal("1e3"),
+  E4:                   new Decimal("1e4"),
+  E5:                   new Decimal("1e5"),
+  E6:                   new Decimal("1e6"),
+  E7:                   new Decimal("1e7"),
+  E8:                   new Decimal("1e8"),
+  E9:                   new Decimal("1e9"),
+  E10:                  new Decimal("1e10"),
+  E12:                  new Decimal("1e12"),
+  E13:                  new Decimal("1e13"),
+  E15:                  new Decimal("1e15"),
+  E18:                  new Decimal("1e18"),
+  E20:                  new Decimal("1e20"),
+  E24:                  new Decimal("1e24"),
+  E25:                  new Decimal("1e25"),
+  E29:                  new Decimal("1e29"),
+  E30:                  new Decimal("1e30"),
+  E31:                  new Decimal("1e31"),
+  E40:                  new Decimal("1e40"),
+  E45:                  new Decimal("1e45"),
+  E50:                  new Decimal("1e50"),
+  E55:                  new Decimal("1e55"),
+  E58:                  new Decimal("1e58"),
+  E60:                  new Decimal("1e60"),
+  E63:                  new Decimal("1e63"),
+  E70:                  new Decimal("1e70"),
+  E75:                  new Decimal("1e75"),
+  E80:                  new Decimal("1e80"),
+  E90:                  new Decimal("1e90"),
+  E100:                 new Decimal("1e100"),
+  E140:                 new Decimal("1e140"),
+  E150:                 new Decimal("1e150"),
+  E160:                 new Decimal("1e160"),
+  E170:                 new Decimal("1e170"),
+  E175:                 new Decimal("1e175"),
+  E200:                 new Decimal("1e200"),
+  E250:                 new Decimal("1e250"),
+  E256:                 new Decimal("1e256"),
+  E260:                 new Decimal("1e260"),
+  E280:                 new Decimal("1e280"),
+  E300:                 new Decimal("1e300"),
+  E308:                 new Decimal("1e308"),
+  E309:                 new Decimal("1e309"),
+  E310:                 new Decimal("1e310"),
+  E315:                 new Decimal("1e315"),
+  E320:                 new Decimal("1e320"),
+  E349:                 new Decimal("1e349"),
+  E400:                 new Decimal("1e400"),
+  E450:                 new Decimal("1e450"),
+  E500:                 new Decimal("1e500"),
+  E530:                 new Decimal("1e530"),
+  E550:                 new Decimal("1e550"),
+  E600:                 new Decimal("1e600"),
+  E616:                 new Decimal("1e616"),
+  E650:                 new Decimal("1e650"),
+  E750:                 new Decimal("1e750"),
+  E850:                 new Decimal("1e850"),
+  E900:                 new Decimal("1e900"),
+  E925:                 new Decimal("1e925"),
+  E975:                 new Decimal("1e975"),
+  E1000:                new Decimal("1e1000"),
+  E1100:                new Decimal("1e1100"),
+  E1200:                new Decimal("1e1200"),
+  E1300:                new Decimal("1e1300"),
+  E1400:                new Decimal("1e1400"),
+  E1500:                new Decimal("1e1500"),
+  E1750:                new Decimal("1e1750"),
+  E1800:                new Decimal("1e1800"),
+  E1900:                new Decimal("1e1900"),
+  E2000:                new Decimal("1e2000"),
+  E2350:                new Decimal("1e2350"),
+  E2400:                new Decimal("1e2400"),
+  E2500:                new Decimal("1e2500"),
+  E2650:                new Decimal("1e2650"),
+  E2700:                new Decimal("1e2700"),
+  E2750:                new Decimal("1e2750"),
+  E2800:                new Decimal("1e2800"),
+  E2900:                new Decimal("1e2900"),
+  E3000:                new Decimal("1e3000"),
+  E3200:                new Decimal("1e3200"),
+  E3350:                new Decimal("1e3350"),
+  E4000:                new Decimal("1e4000"),
+  E5000:                new Decimal("1e5000"),
+  E6000:                new Decimal("1e6000"),
+  E8000:                new Decimal("1e8000"),
+  E9000:                new Decimal("1e9000"),
+  E10000:               new Decimal("1e10000"),
+  E10500:               new Decimal("1e10500"),
+  E11000:               new Decimal("1e11000"),
+  E11111:               new Decimal("1e11111"),
+  E11150:               new Decimal("1e11150"),
+  E11200:               new Decimal("1e11200"),
+  E12000:               new Decimal("1e12000"),
+  E13000:               new Decimal("1e13000"),
+  E14000:               new Decimal("1e14000"),
+  E16500:               new Decimal("1e16500"),
+  E17500:               new Decimal("1e17500"),
+  E18000:               new Decimal("1e18000"),
+  E20000:               new Decimal("1e20000"),
+  E22500:               new Decimal("1e22500"),
+  E23000:               new Decimal("1e23000"),
+  E27000:               new Decimal("1e27000"),
+  E28000:               new Decimal("1e28000"),
+  E30000:               new Decimal("1e30000"),
+  E45000:               new Decimal("1e45000"),
+  E54000:               new Decimal("1e54000"),
+  E60000:               new Decimal("1e60000"),
+  E100000:              new Decimal("1e100000"),
+  E110000:              new Decimal("1e110000"),
+  E164000:              new Decimal("1e164000"),
+  E200000:              new Decimal("1e200000"),
+  E201600:              new Decimal("1e201600"),
+  E208000:              new Decimal("1e208000"),
+  E210000:              new Decimal("1e210000"),
+  E256000:              new Decimal("1e256000"),
+  E300000:              new Decimal("1e300000"),
+  E320000:              new Decimal("1e320000"),
+  E500000:              new Decimal("1e500000"),
+  E1E6:                 new Decimal("1e1000000"),
+  E2_5E6:               new Decimal("1e2500000"),
+  E3E6:                 new Decimal("1e3000000"),
+  E6E6:                 new Decimal("1e6000000"),
+  E1E7:                 new Decimal("1e10000000"),
+  E2E7:                 new Decimal("1e20000000"),
+  E4E7:                 new Decimal("1e40000000"),
+  E6E7:                 new Decimal("1e60000000"),
+  E1E8:                 new Decimal("1e100000000"),
+  E1_5E12:              new Decimal("1e1500000000000"),
+  E1E15:                new Decimal("1e1000000000000000"),
 
-  get description() {
-    return typeof this.config.description === "function" ? this.config.description() : this.config.description;
-  }
-
-  onUnlock() {
-    this.config.onUnlock?.();
-  }
-}
-
-export const TeresaUnlocks = mapGameDataToObject(
-  GameDatabase.celestials.teresa.unlocks,
-  config => new TeresaUnlockState(config)
-);
-
-export const PerkShopUpgrade = mapGameDataToObject(
-  GameDatabase.celestials.perkShop,
-  config => new PerkShopUpgradeState(config)
-);
-
-EventHub.logic.on(GAME_EVENT.TAB_CHANGED, () => {
-  if (Tab.celestials.teresa.isOpen) Teresa.quotes.initial.show();
+  // Special case values
+  NUMSAFE:              new Decimal(Number.MAX_SAFE_INTEGER),
+  NUMMAX:               new Decimal(Number.MAX_VALUE),
+  BIMAX:                new Decimal("e9e15"),
+  BEMAX:                new Decimal("10^^9000000000000000")
 });
 
-EventHub.logic.on(GAME_EVENT.GAME_LOAD, () => Teresa.checkForUnlocks());
+window.AUTOBUYER_MODE = {
+  BUY_SINGLE: 1,
+  BUY_10: 10,
+  BUY_MAX: 100,
+};
+
+window.AUTO_CRUNCH_MODE = {
+  AMOUNT: 0,
+  TIME: 1,
+  X_HIGHEST: 2
+};
+
+window.AUTO_ETERNITY_MODE = {
+  AMOUNT: 0,
+  TIME: 1,
+  X_HIGHEST: 2
+};
+
+window.AUTO_REALITY_MODE = {
+  RM: 0,
+  GLYPH: 1,
+  EITHER: 2,
+  BOTH: 3,
+  TIME: 4,
+  RELIC_SHARD: 5,
+};
+
+window.RECENT_PRESTIGE_RESOURCE = {
+  ABSOLUTE_GAIN: 0,
+  RATE: 1,
+  CURRENCY: 2,
+  PRESTIGE_COUNT: 3,
+};
+
+// Used as drag and drop data type
+window.GLYPH_MIME_TYPE = "text/x-ivark-glyph";
+
+window.GLYPH_BG_SETTING = {
+  AUTO: 0,
+  LIGHT: 1,
+  DARK: 2,
+};
+
+window.ALTERATION_TYPE = {
+  ADDITION: 1,
+  EMPOWER: 2,
+  BOOST: 3
+};
+
+window.BLACK_HOLE_PAUSE_MODE = {
+  NO_PAUSE: 0,
+  PAUSE_BEFORE_BH1: 1,
+  PAUSE_BEFORE_BH2: 2,
+};
+
+window.GLYPH_SIDEBAR_MODE = {
+  INVENTORY_MANAGEMENT: 0,
+  FILTER_SETTINGS: 1,
+  SAVED_SETS: 2,
+  SACRIFICE_TYPE: 3,
+};
+
+window.AUTO_GLYPH_SCORE = {
+  LOWEST_SACRIFICE: 0,
+  EFFECT_COUNT: 1,
+  RARITY_THRESHOLD: 2,
+  SPECIFIED_EFFECT: 3,
+  EFFECT_SCORE: 4,
+  LOWEST_ALCHEMY: 5,
+  ALCHEMY_VALUE: 6
+};
+
+window.AUTO_GLYPH_REJECT = {
+  SACRIFICE: 0,
+  REFINE: 1,
+  REFINE_TO_CAP: 2,
+};
+
+window.TIME_STUDY_PATH = {
+  NONE: 0,
+  ANTIMATTER_DIM: 1,
+  INFINITY_DIM: 2,
+  TIME_DIM: 3,
+  ACTIVE: 4,
+  PASSIVE: 5,
+  IDLE: 6,
+  LIGHT: 7,
+  DARK: 8
+};
+
+window.TIME_STUDY_TYPE = {
+  NORMAL: 0,
+  ETERNITY_CHALLENGE: 1,
+  DILATION: 2,
+  TRIAD: 3
+};
+
+window.TS_REQUIREMENT_TYPE = {
+  AT_LEAST_ONE: 0,
+  ALL: 1,
+  DIMENSION_PATH: 2,
+};
+
+window.ALCHEMY_RESOURCE = {
+  POWER: 0,
+  INFINITY: 1,
+  TIME: 2,
+  REPLICATION: 3,
+  DILATION: 4,
+  CARDINALITY: 5,
+  ETERNITY: 6,
+  DIMENSIONALITY: 7,
+  INFLATION: 8,
+  ALTERNATION: 9,
+  EFFARIG: 10,
+  SYNERGISM: 11,
+  MOMENTUM: 12,
+  DECOHERENCE: 13,
+  EXPONENTIAL: 14,
+  FORCE: 15,
+  UNCOUNTABILITY: 16,
+  BOUNDLESS: 17,
+  MULTIVERSAL: 18,
+  UNPREDICTABILITY: 19,
+  REALITY: 20
+};
+
+window.SINGULARITY_MILESTONE_RESOURCE = {
+  SINGULARITIES: 0,
+  CONDENSE_COUNT: 1,
+  MANUAL_TIME: 2,
+  AUTO_TIME: 3,
+};
+
+window.SINGULARITY_MILESTONE_SORT = {
+  SINGULARITIES_TO_NEXT: 0,
+  CURRENT_COMPLETIONS: 1,
+  PERCENT_COMPLETIONS: 2,
+  FINAL_COMPLETION: 3,
+  MOST_RECENT: 4,
+};
+
+window.COMPLETED_MILESTONES = {
+  FIRST: 0,
+  LAST: 1,
+  IGNORED: 2,
+};
+
+window.SORT_ORDER = {
+  ASCENDING: 0,
+  DESCENDING: 1,
+};
+
+// One-indexed and ordered to simplify code elsewhere, do not change to be zero-indexed or reorder
+window.PROGRESS_STAGE = {
+  PRE_INFINITY: 1,
+
+  EARLY_INFINITY: 2,
+  BREAK_INFINITY: 3,
+  REPLICANTI: 4,
+
+  EARLY_ETERNITY: 5,
+  ETERNITY_CHALLENGES: 6,
+  EARLY_DILATION: 7,
+  LATE_ETERNITY: 8,
+
+  EARLY_REALITY: 9,
+
+  TERESA: 10,
+  EFFARIG: 11,
+  ENSLAVED: 12,
+  V: 13,
+  RA: 14,
+  IMAGINARY_MACHINES: 15,
+  LAITELA: 16,
+  PELLE: 17,
+};
+
+window.SPEEDRUN_SEED_STATE = {
+  UNKNOWN: 0,
+  FIXED: 1,
+  RANDOM: 2,
+  PLAYER: 3,
+};
+
+// We're just going to use cloneDeep from lodash, since its better
+window.cloneDeep = value => cloneDeep(value);
+
+//
+// window.cloneDeep = function cloneDeep(obj) {
+// if (Array.isArray(obj)) {
+//     return obj.map(i => cloneDeep(i));
+// }
+// if (obj instanceof Decimal) {
+//     return new Decimal(obj);
+// }
+// if (typeof obj === "object" && obj !== null) {
+//     return Object.fromEntries(Object.entries(obj).map(([i, j]) => [i, cloneDeep(j)]));
+// }
+// return obj;
+// };
+//
